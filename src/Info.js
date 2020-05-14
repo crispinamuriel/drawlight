d8a8a0244a6d55eee6a02b4e49c0395e618c714e
crispinamuriel <crispinamuriel@gmail.com>
2020-05-14 13:22:56 -0400

    removed app text in html title

--- src/Info.js
+++ src/Info.js
@@ -4,9 +4,14 @@ export default function Info() {
   return (
     <div className="info">
       <div className="text-container">
-        <h1>Join Drawlight for Beta Testing</h1>
+        <h1>
+          Join Drawlight for <br></br>Beta Testing
+        </h1>
         <div className="tall">
-          <p>Make every space your canvas! With 3D art and Augmented Reality brushes and textures. Enjoy it with friends or by yourself.</p>
+          <p>
+            Make every space your canvas! With 3D art and Augmented Reality
+            brushes and textures.<br></br> Enjoy it with friends or by yourself.
+          </p>
         </div>
         <hr className="tall"></hr>
         <a href="https://www.google.com">
